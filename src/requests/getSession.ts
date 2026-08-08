8a3e97cc2f9f44199ff550c9deafffa315bc75a3
RafaTheScripter <nativaeleven@gmail.com>
2026-08-08 15:30:27 -0300

    Pegando o nome de usuário quando ele está logado e colocando no header

--- src/requests/getSession.ts
+++ src/requests/getSession.ts
@@ -1,10 +1,24 @@
 import { URL } from "../url.js";
 
 export async function getSession(){
+    const headerLink = document.querySelector('[data-header-link]')
     const response = await fetch(URL+'auth/session',{
         method:'GET',
         credentials:'include'
     })
     const dados = await response.json()
+    
+    console.log(dados)
+
+    if(response.status && response.status === 200){
+        if(headerLink instanceof HTMLElement){
+            headerLink.innerText = dados.username
+        }
+    }else{
+        if(headerLink instanceof HTMLElement){
+            headerLink.innerText = 'Login / Criar'
+        }
+    }
+
     return response.status
 }
