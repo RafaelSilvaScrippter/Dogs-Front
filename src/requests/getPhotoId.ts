1dba9bf52e762b183f8fe40d30093768f27a5aaa
RafaTheScripter <nativaeleven@gmail.com>
2026-08-09 14:30:48 -0300

    Arrumando as páginas html  dinamicas das postagens de usuário e postagem da foto do cachorro

--- src/requests/getPhotoId.ts
+++ src/requests/getPhotoId.ts
@@ -1,4 +1,6 @@
 import { URL } from "../url.js";
+import { PostComments } from "./fetchPostComment.js";
+import { getSession } from "./getSession.js";
 
 interface DataPhoto {
     id:number;
@@ -14,12 +16,12 @@ interface DataPhoto {
 export async function getPhotoId(){
     const dataMainPhoto = document.querySelector('[data-main-photo]');
     const url = window.location.search;
-    const id = url.replace('?','').replace('.html','');
+    const id = url.replace('?id=','')
     const response = await fetch(URL+`get/photo/${id}`);
-    const dados:Record<string,DataPhoto>  = await response.json();
-    
-    console.log(dados);
+    const dados:Record<string,DataPhoto> & Record<string,any>  = await response.json();
     if(!dataMainPhoto) return;
+
+    dataMainPhoto.innerHTML = ''
 
     if(dataMainPhoto &&
         typeof dados === 'object' &&
@@ -39,22 +41,52 @@ export async function getPhotoId(){
 
     <img src='${URL}send/${dados.post.src}' />
 
-      <div class='modal-aside-dados bg-initial'>
+        <div data-conteudo-photo class='modal-aside-dados bg-initial'>
             <div class='author-e-views'>
-                <a href='../pages/author?${dados.post.user_name}.html'>
+                <a href='../../pages/author.html?user=${dados.post.user_name}'>
                     <span class='author'>@${dados.post.user_name}</span>
                 </a>
                 <span class='views-modal'>${dados.post.views}</span>
             </div>
-            <a href='../pages/photo?${dados.post.id}.html'>
-                <h2 class='titulo-modal'>${dados.post.nome}</h2>
-                </a>
-                <div class='dados-item-dog'>
+            <a href='../../pages/photo.html?id=${dados.post.id}'>
+             <h2 class='titulo-modal'>${dados.post.nome}</h2>
+            </a>
+            <div class='dados-item-dog'>
                 <span class='barra-esq-dir'>${dados.post.peso} Kg</span>
                 <span>${dados.post.idade} anos</span>
-                </div>
-                </div>
-                
-                `;
+            </div>
+            <div div-comments class="div-comentrarios">
+                ${dados.comentarios.map((item:Record<string,any>) =>{
+                    
+                    return /*HTML */`
+                    <div class='item-div-comment'>
+                        <span>${item.user_name}:</span>` + /*HTML */`<p>${item.comment}</p>
+                    </div>
+                    `
+                })}
+            </div>
+            <div>
+            
+                ${await getSession() === 200 ?
+                /*HTML */
+                `
+                    <div class='div-comment-input'>
+                        <input data-input-comment class='input-env-commnet' type='text' name='comment' id='comment' />
+                        <img data-env-comment src='../../../assets/enviar.svg'/>
+                    </div>
+                `
+                :
+                    ''  }
+            </div>
+        </div>
+    `;
+     const htmlElementPost = document.querySelector('[data-conteudo-photo]');
+        if(htmlElementPost instanceof HTMLElement){
+             new PostComments(Number(id),htmlElementPost).init();
+        }
+       const e =  htmlElementPost?.querySelector('[div-comments]')
+       e?.childNodes.forEach((t) =>{
+        t.remove()
+       })
     }
 }
